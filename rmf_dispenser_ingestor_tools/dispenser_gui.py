eed7f54f7f383a88bb30e58653c00d72c5ec7115
Robert <valnerrobert@gmail.com>
2022-02-19 18:42:24 +0200

    Making sure that same requests are not served multiple times

--- rmf_dispenser_ingestor_tools/dispenser_gui.py
+++ rmf_dispenser_ingestor_tools/dispenser_gui.py
@@ -27,17 +27,17 @@ class DispenserTestGui(Node):
 
         # Initialize the buttons
         self.user_button = Button(self.window, text="", command=self.user_button_click, height = 20, width = 30)
-        # self.user_button.grid(column=0, row=4)
-
         self.lbl_state_name.pack(side=LEFT)
         self.lbl_state_value.pack(side=LEFT)
         self.user_button.pack()
 
-        # Initialize the dispenser state
+        # Initialize the dispenser state and list of served requests
         self.dispenser_state_msg = DispenserState()
         self.dispenser_state_msg.guid = self.dispenser_name
         self.dispenser_state_msg.mode = DispenserState().IDLE
         self.dispenser_state_mutex = Lock()
+        self.served_requests_mutex = Lock()
+        self.served_requests = []
 
         # Set up publishers, subscribers, etc
         self.dispenser_state_timer = self.create_timer(2.0, self.dispenser_state_timer_callback)
@@ -55,9 +55,14 @@ class DispenserTestGui(Node):
         self.dispenser_state_mutex.release()
 
     def dispenser_requests_callback(self, msg):
-        print("received a dispensing request")
+        print("received a dispensing request for '" + msg.request_guid + "'")
         self.dispenser_state_mutex.acquire()
-        if self.dispenser_state_msg.mode == DispenserState().IDLE and msg.target_guid == self.dispenser_name:
+
+        # Make sure that the dispenser is idling; the request is for this dispenser and 
+        # this request has not been served before
+        if self.dispenser_state_msg.mode == DispenserState().IDLE \
+        and msg.target_guid == self.dispenser_name \
+        and self.is_new_request(msg.request_guid):
             self.dispenser_state_msg.mode = DispenserState().BUSY
             self.lbl_state_value.configure(text="DISPENSE")
             self.dispenser_state_msg.request_guid_queue.append(msg.request_guid)
@@ -74,14 +79,14 @@ class DispenserTestGui(Node):
     def user_button_click(self):
         self.dispenser_state_mutex.acquire()
         if self.dispenser_state_msg.mode == DispenserState().BUSY:
-            self.dispenser_state_msg.mode = DispenserState().IDLE
+            # Set the dispenser temporarly offline, so that the "button blikning loop"
+            # can be broken without accepting new requests
+            self.dispenser_state_msg.mode = DispenserState().OFFLINE
             self.lbl_state_value.configure(text="IDLE")
         self.dispenser_state_mutex.release()
 
     def blink_user_button(self):
-        self.blink_the_button = True
         print("starting to blink")
-
         while self.dispenser_state_msg.mode == DispenserState().BUSY:
             self.user_button["background"] = "yellow"
             self.user_button["activebackground"] = "yellow"
@@ -92,25 +97,45 @@ class DispenserTestGui(Node):
             sleep(1)
         
         print("blinking stopped")
+
+        # Send a result to RMF
         dispenser_result_msg = DispenserResult()
         dispenser_result_msg.source_guid = self.dispenser_name
         dispenser_result_msg.status = DispenserResult().SUCCESS
+        dispenser_result_msg.time = self.get_clock().now().to_msg()
 
         self.dispenser_state_mutex.acquire()
         dispenser_result_msg.request_guid = self.dispenser_state_msg.request_guid_queue.pop(0)
-        self.dispenser_state_mutex.release()
+        self.add_served_request(dispenser_result_msg.request_guid)
         self.dispenser_result_pub.publish(dispenser_result_msg)
+        self.dispenser_state_msg.mode = DispenserState().IDLE
+        self.dispenser_state_mutex.release()
+
+    def is_new_request(self, request_guid):
+        self.served_requests_mutex.acquire()
+        exists = request_guid in self.served_requests
+        self.served_requests_mutex.release()
+        return not exists
+
+    def add_served_request(self, request_guid):
+        self.served_requests_mutex.acquire()
+        self.served_requests.append(request_guid)
+        self.served_requests_mutex.release()
 
     def show(self):
         self.window.mainloop()
 
 def main(argv=sys.argv):
+    # Init ros
     rclpy.init(args=sys.argv)
 
+    # Get the name of the dispenser via arguments
     args_without_ros = rclpy.utilities.remove_ros_args(sys.argv)
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--name', required=True, type=str, help='Name of the dispenser')
-    args = parser.parse_args(argv[1:])    
+    args = parser.parse_args(argv[1:])
+
+    # Initialize the dispenser
     dispenser_test_gui = DispenserTestGui(args.name)
 
     # I wasn't able to find an async spinner, thus starting the spinner in its own thread
